a346e025d3b08b55de193817352cc1f09b514dac
Tom Bannister <tomb@Adprinciples.co.uk>
2017-10-20 16:57:57 +0100

    Start get unread messages controller function

--- src/MadeByTom/NotificationBundle/Controller/NotificationController.php
+++ src/MadeByTom/NotificationBundle/Controller/NotificationController.php
@@ -2,6 +2,7 @@
 
 namespace MadeByTom\NotificationBundle\Controller;
 
+use MadeByTom\NotificationBundle\Entity\Message;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -15,8 +16,21 @@ class NotificationController extends Controller
         return $this->render('MadeByTomNotificationBundle:Default:index.html.twig');
     }
 
+    /**
+     * @param int $max
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function getUnreadMessages($max = 3)
     {
+
+        $unreadMessages = $this->getDoctrine()
+                               ->getRepository(Message::class)
+                               ->findOneBy(array('read' => 0));
+
+        return $this->render(
+            '@MadeByTomCore/partials/navbar.html.twig',
+            array('unreadMessages' => $unreadMessages)
+        );
 
     }
 }
