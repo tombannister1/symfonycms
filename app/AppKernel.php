9341148d77d4b0302c77d09490eb217e864f3d6f
Tom Bannister <tomb@Adprinciples.co.uk>
2017-10-23 17:00:38 +0100

    Set up base page bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,12 +16,12 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new MadeByTom\CoreBundle\AppBundle(),
-
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new MadeByTom\DashboardBundle\MadeByTomDashboardBundle(),
             new MadeByTom\NotificationBundle\MadeByTomNotificationBundle(),
             new Knp\Bundle\TimeBundle\KnpTimeBundle(),
+            new PageBundle\PageBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
